64ff8eaf74421c86442caeefd4790e732f358266
HengChamroeurn <51738736+zTwinz@users.noreply.github.com>
2020-06-10 17:14:13 +0700

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,119 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Home(),
+    home: Profile(),
   debugShowCheckedModeBanner: false,
 ));
-class Home extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  int age = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("My First App"),
+        title: Text("Twin Card ID"),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Center(
-        child: RaisedButton.icon(
-          onPressed: (){},
-          icon: Icon(
-            Icons.access_alarm,
-            color: Colors.yellow,
-          ),
-          label: Text("Alarm",style: TextStyle(color: Colors.cyanAccent),),
-          color: Colors.green,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/flutterLogo.png"),
+                radius: 40.0,
+                backgroundColor: Colors.red,
+              ),
+            ),
+            Divider(
+              height: 70,
+              color: Colors.red,
+            ),
+            Text(
+              "FirstName",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Heng",
+              style: TextStyle(
+                letterSpacing: 2.0,
+                color: Colors.orange,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              "LastName",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Chamroeurn",
+              style: TextStyle(
+                letterSpacing: 2.0,
+                color: Colors.orange,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              "Age",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "$age",
+              style: TextStyle(
+                letterSpacing: 2.0,
+                color: Colors.orange,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(Icons.mail,color: Colors.deepOrange,),
+                SizedBox(width: 10.0),
+                Text("hengChamroeurn@gmail.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        print("Clicked!!");
-      },
-        child: Text("Click!"),
-        backgroundColor: Colors.red[600],
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState((){
+            age += 1;
+          });
+          },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
     );
   }
